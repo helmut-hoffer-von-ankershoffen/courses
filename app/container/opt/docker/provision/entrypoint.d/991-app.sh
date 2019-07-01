95ac77021824c8f8d6e25d331bd32710f00db854
Helmut Hoffer von Ankershoffen <helmuthva@googlemail.com>
2019-07-01 21:37:15 +0200

    feat: autoinstall

--- app/container/opt/docker/provision/entrypoint.d/991-app.sh
+++ app/container/opt/docker/provision/entrypoint.d/991-app.sh
@@ -2,22 +2,22 @@
 
 echo "Provisioning app ..."
 
-# Create necessary directories if missing
+# Create necessary directories if missing and fix permissions
 echo "Creating necessary directories ..."
 mkdir -p /app/web/app/uploads
-
-## Fix permissions
 echo "Setting permisssions ..."
 chmod -R 777 /app/web/app/uploads
 
 # Execute as user application
 su application << 'EOSU'
 
-CMD_APP="php /app/vendor/bin/wp"
-
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 ssh-keyscan -t rsa,dsa github.com >> ~/.ssh/known_hosts
+
+if [! $(vendor/bin/wp core is-installed)]; then
+  vendor/bin/wp core install --url=${APP_WP_HOME} --title=Courses --admin_user=hhva --admin_password=secret --admin_email=helmuthva@googlemail.com --skip-email
+fi
 
 ## Dump autoloader
 echo "Dumping optimized autoloader"
@@ -27,7 +27,7 @@ cd /app && composer dump-autoload -o --apcu
 EOSU
 
 ## Fix permissions again
-echo "Setting permisssions again ..."
+echo "Fixing permisssions again ..."
 chmod -R 777 /app/web/app/uploads
 
 echo "Provisioning app done."
